03178a1f81aebb90fac25594d500e4c95934814b
NaO3430 <fariytail1113@gmail.com>
2023-09-20 11:07:32 +0900

    削除ボタンの動作実装

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -7,19 +7,33 @@ import { Task } from './types';
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
+
   const addTask = (text: string) => {
+    const maxId =
+      tasks.length > 0 ? Math.max(...tasks.map((task) => task.id)) : 0;
     const newTask: Task = {
-      id: tasks.length + 1,
+      id: maxId + 1,
       text: text,
       status: '作業中',
     };
     setTasks((prevTasks) => [...prevTasks, newTask]);
   };
+
+  const deleteTask = (id: number) => {
+    const updatedTasks = tasks
+      .filter((task) => task.id !== id)
+      .map((task, index) => ({
+        ...task,
+        id: index + 1,
+      }));
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
       <Title />
       <Radiobutton />
-      <TaskTable tasks={tasks} />
+      <TaskTable tasks={tasks} onTaskDelete={deleteTask} />
       <TaskInputForm onAdd={addTask} />
     </div>
   );
--- todolist/src/TaskTable.tsx
+++ todolist/src/TaskTable.tsx
@@ -1,35 +1,36 @@
 import React from 'react';
 import { Task } from './types';
+import DeleteButton from './DeleteButton';
+
 type TaskTableProps = {
   tasks: Task[];
+  onTaskDelete: (id: number) => void;
 };
 
-function TaskTable({ tasks }: TaskTableProps) {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>コメント</th>
-          <th>状態</th>
+const TaskTable: React.FC<TaskTableProps> = ({ tasks, onTaskDelete }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>コメント</th>
+        <th>状態</th>
+      </tr>
+    </thead>
+    <tbody>
+      {tasks.map((task) => (
+        <tr key={task.id}>
+          <td>{task.id}</td>
+          <td>{task.text}</td>
+          <td>
+            <button>{task.status}</button>
+          </td>
+          <td>
+            <DeleteButton taskId={task.id} onDelete={onTaskDelete} />
+          </td>
         </tr>
-      </thead>
-      <tbody>
-        {tasks.map((task) => (
-          <tr key={task.id}>
-            <td>{task.id}</td>
-            <td>{task.text}</td>
-            <td>
-              <button>{task.status}</button>
-            </td>
-            <td>
-              <button>削除</button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
+      ))}
+    </tbody>
+  </table>
+);
 
 export default TaskTable;
